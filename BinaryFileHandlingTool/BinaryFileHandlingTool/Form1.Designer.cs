2d67e667965262b55f43311eb0ba1e35ec641ddd
Omar Jabr <omarjabr165@gmail.com>
2018-10-21 15:45:47 +0200

    Second Change

--- BinaryFileHandlingTool/BinaryFileHandlingTool/Form1.Designer.cs
+++ BinaryFileHandlingTool/BinaryFileHandlingTool/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtString = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(322, 324);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(121, 50);
+            this.btnSave.TabIndex = 17;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.Location = new System.Drawing.Point(14, 15);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(133, 73);
+            this.btnOpenFile.TabIndex = 16;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtString
+            // 
+            this.txtString.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtString.Location = new System.Drawing.Point(14, 95);
+            this.txtString.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(429, 221);
+            this.txtString.TabIndex = 15;
+            this.txtString.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(457, 389);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtString);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1Binary File Handling Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.RichTextBox txtString;
     }
 }
 
--- BinaryFileHandlingTool/BinaryFileHandlingTool/Form1.cs
+++ BinaryFileHandlingTool/BinaryFileHandlingTool/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BinaryFileHandlingTool
@@ -15,6 +11,47 @@ namespace BinaryFileHandlingTool
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Convert Class Binary To String
+        public static string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string asci = File.ReadAllText(ofd.FileName);
+                string s = BinaryToString(asci);
+                txtString.Text = s;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string value = "";
+            if (inputBox.InputBox("New file", "New file name:", ref value) == DialogResult.OK)
+            {
+                using (StreamWriter stw = new StreamWriter(value + ".txt"))
+                {
+                    stw.WriteLine(txtString.Text);
+                    MessageBox.Show("File saved successfully");
+                    txtString.Text = "";
+                }
+            }
         }
     }
 }
