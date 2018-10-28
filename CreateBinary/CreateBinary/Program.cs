8cd9a066b824911f94a175d214664f593ed2a39c
Omar Jabr <omarjabr165@gmail.com>
2018-10-28 13:27:23 +0100

    Create Binary

---
+++ CreateBinary/CreateBinary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CreateBinary
+{
+    class Program
+    {
+        static void CreateBinary()
+        {
+            Console.WriteLine("Name your binary file: ");
+            string fileName = Console.ReadLine();
+            FileStream fs = new FileStream(fileName + ".bin", FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            Console.WriteLine("Enter the text to be in the file:");
+            string fileContent = Console.ReadLine();
+            bw.Write(fileContent);
+
+            bw.Close();
+            fs.Close();
+        }
+
+        static void ReadBinary()
+        {
+            Console.WriteLine("Which file do you want to preview: ");
+            string fileName = Console.ReadLine();
+            FileStream fs = new FileStream(fileName + ".bin", FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+
+            string read = br.ReadString();
+            Console.WriteLine(read);
+        }
+
+        static void Main(string[] args)
+        {
+            CreateBinary();
+            ReadBinary();
+            Console.ReadKey();
+        }
+    }
+}
